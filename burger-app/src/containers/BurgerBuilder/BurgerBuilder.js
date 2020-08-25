bcf18c4e7aff4cd00e52e3233796a123c9c2d183
Tomasz Bochnak <tomaszbochnak00@gmail.com>
2020-08-26 00:33:46 +0200

    8-9

---
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+
+    render() {
+        return (
+            <Aux>
+                <div>Burger Graphic</div>
+                <div>Build Area</div>
+            </Aux>
+        );
+    }
+    
+}
+
+export default BurgerBuilder;
---
+++ burger-app/src/hoc/Auxiliary.js
@@ -0,0 +1,5 @@
+const aux = (props) => {
+    return props.children; // it is an alternative to div element
+}
+
+export default aux;
